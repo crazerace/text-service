c5ab324514d0bbf84d2df20dec738ed6df5b6281
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-06-03 23:05:29 +0200

    Added TextGroup model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -11,7 +11,7 @@ class Language(db.Model):  # type: ignore
     created_at: datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self) -> str:
-        return f"Language(id={self.id} created_at={self.created_at})"
+        return f"Language(id={self.id}, created_at={self.created_at})"
 
 
 class TranslatedText(db.Model):  # type: ignore
@@ -26,11 +26,11 @@ class TranslatedText(db.Model):  # type: ignore
 
     def __repr__(self) -> str:
         return (
-            f"TranslatedText(id={self.id} "
-            f"key={self.key} "
-            f"language={self.language} "
-            f"value={self.value} "
-            f"created_at={self.created_at} "
+            f"TranslatedText(id={self.id}, "
+            f"key={self.key}, "
+            f"language={self.language}, "
+            f"value={self.value}, "
+            f"created_at={self.created_at}, "
             f"last_modified_at={self.last_modified_at})"
         )
 
@@ -43,14 +43,19 @@ class Group(db.Model):  # type: ignore
         return f"Group(id={self.id} created_at={self.created_at})"
 
 
-"""
 class TextGroup(db.Model):  # type: ignore
     __table_args__ = (
         db.UniqueConstraint("text_key", "group_id", name="unique_text_key_group_id"),
     )
+    id: int = db.Column(db.Integer, primary_key=True)
     text_key: int = db.Column(db.String(255), nullable=False)
     group_id: int = db.Column(db.String(255), nullable=False)
     created_at: datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self) -> str:
-        return f"TextGroup(text_key={self.text_key} group_id={self.group_id} created_at={self.created_at})"""
+        return (
+            f"TextGroup(id={self.id}, "
+            f"text_key={self.text_key}, "
+            f"group_id={self.group_id} ,"
+            f"created_at={self.created_at})"
+        )
