5aff621a97ba9b6d7c99ca5feb6a3064cea8297e
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-06-29 21:45:47 +0200

    Fixed health check function

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,3 +1,6 @@
+# Standard library
+from unittest.mock import patch
+
 # 3rd party modules
 from crazerace.http import status
 
@@ -5,10 +8,16 @@ from crazerace.http import status
 from tests import TestEnvironment, JSON
 
 
-def test_get_health():
+def test_get_health_success():
     with TestEnvironment() as client:
         res = client.get("/health", content_type=JSON)
-        body = res.get_json()
         assert res.status_code == status.HTTP_200_OK
         assert res.get_json()["status"] == "UP"
         assert res.get_json()["db"] == "UP"
+
+
+def test_get_health_fail():
+    with patch("app.service.health._db_connected", return_value=("DOWN", False)):
+        with TestEnvironment() as client:
+            res = client.get("/health", content_type=JSON)
+            assert res.status_code == status.HTTP_503_SERVICE_UNAVAILIBLE
